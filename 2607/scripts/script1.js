27e66fc15b42cc3e7a61cc019c1ab74979d3e264
Anastasija Baldujeva <anastaacia@gmail.com>
2022-08-07 18:33:26 +0300

    first JS exercise 2607

---
+++ 2607/scripts/script1.js
@@ -0,0 +1,25 @@
+alert("Hello World");
+
+//addition/substraction/multiplication/division
+let a = prompt("Please enter an integer number A:");
+
+let b = prompt("Please enter an integer number B:");
+
+let plus = Number.parseInt(a) + Number.parseInt(b);
+
+let minus = a - b;
+
+let multiply = a * b;
+
+let divide = a / b;
+
+alert("The result of addition is " + plus);
+alert("The result of substraction is " + minus);
+alert("The result of multiplication is " + multiply);
+alert("The result of division is " + divide);
+
+//Name Surname
+let firstName = prompt("Enter you first name: ");
+let lastName = prompt("Enter your last name: ");
+
+alert("Hello " + firstName + " " + lastName);
